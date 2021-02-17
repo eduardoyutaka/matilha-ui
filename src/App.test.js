910e9e5cf4b68691253e4d3ceb43c514950c63a8
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2021-02-17 15:09:29 -0300

    Install enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+// setup file
+import { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+// test file
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test('Button renders on screen', () => {
+  const wrapper = shallow(<Foo />);
+
+
+
+
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+ 
+});
+
+test('Calls action when clicked', () => {
+  // setup
+  // execise SUT (system under test)
+  // evaluate
 });
